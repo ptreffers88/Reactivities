fc190016cc73ac312b29376225431c90e32b11c8
Paul <paul@bluenotion.nl>
2022-12-29 10:59:28 +0100

    End of section 9

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import 'semantic-ui-css/semantic.min.css'
-import './app/layout/styles.css';
-import reportWebVitals from './reportWebVitals';
-import { store, StoreContext } from './app/stores/store';
-import {RouterProvider} from 'react-router-dom';
-import { router } from './app/router/Routes';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "semantic-ui-css/semantic.min.css";
+import "react-calendar/dist/Calendar.css";
+import "./app/layout/styles.css";
+import reportWebVitals from "./reportWebVitals";
+import { store, StoreContext } from "./app/stores/store";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./app/router/Routes";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <StoreContext.Provider value={store}>
